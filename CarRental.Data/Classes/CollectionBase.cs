6f17ddd993a6cabbfa0a8a7520c51c90d5e0d8a4
cCMartinF <carlmartinjonsson92@gmail.com>
2023-09-26 17:30:15 +0200

    2/3 db completed. Ibooking not comp.

--- CarRental.Data/Classes/CollectionBase.cs
+++ CarRental.Data/Classes/CollectionBase.cs
@@ -3,6 +3,7 @@ using CarRental.Common.Enum;
 using CarRental.Common.Interface;
 using CarRental.Data.Interface;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace CarRental.Data.Classes;
 
@@ -17,7 +18,7 @@ public class CollectionBase : IData
     void SeedData() 
     {
         var rentDateOne = new DateTime(2023, 9, 10);
-        var rentDateTwo = new DateTime(2023, 9, 12);
+        var rentDateTwo = new DateTime(2023, 9, 12); 
         var returnedDate = new DateTime(2023, 9, 25);
 
         _customer.Add(new Customer(6407070001, "Johan", "Lager"));
@@ -26,7 +27,10 @@ public class CollectionBase : IData
         _vehicles.Add(new Car("DEF456", "BMW", 2000, 1.5, VehicleType.Sedan, 200, VehicleStatus.Booked));
         _vehicles.Add(new Car("GHI789", "Suzuki", 20000, 2, VehicleType.Motorcycle, 300, VehicleStatus.Available));
         _vehicles.Add(new Car("JKL012", "Yamaha", 300, 3, VehicleType.Motorcycle, 400, VehicleStatus.Booked));
+        var test = _vehicles.First(v => v.regNr == "DEF456");
+        var testTwo = _customer.First(v => v.sSD == 6407070001);
 
+        //_booking.Add(new Booking(test, testTwo, 100, rentDateTwo, null ))
 
         //var carRentedOne = _vehicles.First();
         //var carRentedTwo = _vehicles.
@@ -36,7 +40,7 @@ public class CollectionBase : IData
     //IEnumerable<string> names = from person in people
     //                            where person.Pets.All(pet => pet.Age > 5)
     //                            select person.LastName;
-
+    
     public IEnumerable<ICustomer> GetCustomers() => _customer;
     public IEnumerable<IVehicle> GetVehicles(VehicleStatus status = VehicleStatus.Booked) => _vehicles;
     public IEnumerable<IBooking> GetBookings() => _booking;
